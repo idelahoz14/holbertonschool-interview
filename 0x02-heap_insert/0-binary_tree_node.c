db1eeff2bf6b611dfb0efed5b183fb2023d1d8fd
idelahoz14 <idelahoz14@cuc.edu.co>
2021-05-06 22:50:28 -0500

    Fixing code(Betty style)

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 	{
-        return(NULL);
+        return (NULL);
     }
 	new->n = value;
 	new->parent = parent;
