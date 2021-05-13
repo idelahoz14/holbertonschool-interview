d1ad16a0e20397523a64f6a1443bb80b90653d37
idelahoz14 <idelahoz14@cuc.edu.co>
2021-05-13 14:54:09 -0500

    Method that calculate the minimum operation

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+def minOperations(n):
+    """Calculates minimum copy all and paste operations to reach n characters
+    Args:
+        n: characters to reach
+    Returns:
+        minimum needed operations
+    """
+    sum = 0
+    i = 2
+    while i <= n:
+        if n % i:
+            i += 1
+        else:
+            n = n / i
+            sum += i
+    if n > 1:
+        sum += n
+    return sum
