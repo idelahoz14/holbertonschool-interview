b382290f3b07942e77c79bd7e786ed8830dd5dca
idelahoz14 <idelahoz14@cuc.edu.co>
2021-06-03 22:42:01 -0500

    Try another way

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -14,19 +14,19 @@ def printall(sig=None, frame=None):
     print("File size: {}".format(size))
     if status_dict['200'] > 0:
         print("200: {}".format(status_dict['200']))
-    if status_dict['301'] > 0:
+    elif status_dict['301'] > 0:
         print("301: {}".format(status_dict['301']))
-    if status_dict['400'] > 0:
+    elif status_dict['400'] > 0:
         print("400: {}".format(status_dict['400']))
-    if status_dict['401'] > 0:
+    elif status_dict['401'] > 0:
         print("401: {}".format(status_dict['401']))
-    if status_dict['403'] > 0:
+    elif status_dict['403'] > 0:
         print("403: {}".format(status_dict['403']))
-    if status_dict['404'] > 0:
+    elif status_dict['404'] > 0:
         print("404: {}".format(status_dict['404']))
-    if status_dict['405'] > 0:
+    elif status_dict['405'] > 0:
         print("405: {}".format(status_dict['405']))
-    if status_dict['500'] > 0:
+    elif status_dict['500'] > 0:
         print("500: {}".format(status_dict['500']))
 
 try:
